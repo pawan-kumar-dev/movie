4f137abe4ea4568e330cd0929619ebf7edcaac9e
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-07-25 23:34:05 +0530

    changes-to-classComponent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import { useCallback, useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import Card from "./Components/Card";
 import Switch from "react-switch";
 
-function App() {
-  const [searchText, setSearchText] = useState("");
-  const [resultCount, setResultCount] = useState("");
-  const [error, setError] = useState("");
-  const [result, setResult] = useState([]);
-  const [isDarkMode, setIsDarkMode] = useState(
-    localStorage.getItem("isDarkMode")
-      ? JSON.parse(localStorage.getItem("isDarkMode"))
-      : false
-  );
-  const fetchMovies = useCallback(() => {
+class App extends React.Component {
+  state = {
+    searchText: "",
+    resultCount: "",
+    error: "",
+    result: [],
+    isDarkMode: false,
+  };
+  componentDidMount = () => {
+    this.setState({
+      isDarkMode: localStorage.getItem("isDarkMode")
+        ? JSON.parse(localStorage.getItem("isDarkMode"))
+        : false,
+    });
+    this.fetchMovies();
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    const { searchText } = this.state;
+    if (prevState.searchText !== searchText) this.fetchMovies();
+  };
+  fetchMovies = () => {
+    const { searchText } = this.state;
     let searchApi = "https://www.omdbapi.com/?apikey=45f0782a&s=war";
     if (searchText) {
       searchApi = `https://www.omdbapi.com/?apikey=45f0782a&s=${searchText}`;
@@ -21,53 +33,64 @@ function App() {
     fetch(searchApi).then((response) => {
       response.json().then((result) => {
         if (result.Error) {
-          setError(result.Error);
-          setResultCount("0");
-          setResult([]);
+          this.setState({
+            error: result.Error,
+            resultCount: "0",
+            result: [],
+          });
         } else {
-          setError("");
-          setResultCount(result.totalResults);
-          setResult(result.Search);
+          this.setState({
+            error: "",
+            resultCount: result.totalResults,
+            result: result.Search,
+          });
         }
       });
     });
-  }, [searchText]);
-  useEffect(() => {
-    fetchMovies();
-  }, [fetchMovies]);
-  return (
-    <div className={`App ${isDarkMode && "dark-app"}`}>
-      <form onSubmit={(e) => e.preventDefault()} className="filter">
-        <h1 className="title">React Movie Search</h1>
-        <input
-          value={searchText}
-          onChange={({ target: { value } }) => setSearchText(value)}
-          className="input"
-          placeholder="Search Title. i.e. Fight Club"
-          type="text"
-          title="Find your title"
-        />
-      </form>
-      <div className="details">
-        <h3>Total Search Result : {resultCount}</h3>
-        {error ? <h3>{error}</h3> : <div />}
-        <Switch
-          onChange={(checked) => {
-            setIsDarkMode(checked);
-            localStorage.setItem("isDarkMode", JSON.stringify(checked));
-          }}
-          checked={isDarkMode}
-          uncheckedIcon={false}
-          checkedIcon={false}
-        />
+  };
+  onDarkModeChange = (checked) => {
+    this.setState(
+      {
+        isDarkMode: checked,
+      },
+      () => localStorage.setItem("isDarkMode", JSON.stringify(checked))
+    );
+  };
+  render() {
+    const { isDarkMode, searchText, result, resultCount, error } = this.state;
+    return (
+      <div className={`App ${isDarkMode && "dark-app"}`}>
+        <form onSubmit={(e) => e.preventDefault()} className="filter">
+          <h1 className="title">React Movie Search</h1>
+          <input
+            value={searchText}
+            onChange={({ target: { value } }) =>
+              this.setState({ searchText: value })
+            }
+            className="input"
+            placeholder="Search Title. i.e. Fight Club"
+            type="text"
+            title="Find your title"
+          />
+        </form>
+        <div className="details">
+          <h3>Total Search Result : {resultCount}</h3>
+          {error ? <h3>{error}</h3> : <div />}
+          <Switch
+            onChange={this.onDarkModeChange}
+            checked={isDarkMode}
+            uncheckedIcon={false}
+            checkedIcon={false}
+          />
+        </div>
+        <div className="movie-list">
+          {result.map((movie) => (
+            <Card key={movie.imdbID} {...movie} />
+          ))}
+        </div>
       </div>
-      <div className="movie-list">
-        {result.map((movie) => (
-          <Card key={movie.imdbID} {...movie} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
